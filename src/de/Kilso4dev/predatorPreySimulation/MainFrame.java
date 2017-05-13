0ac5034ebd665e84f5ed2729639b07923d6b1c9c
Robert Reuter <denib.qatlh@t-online.de>
2017-05-13 17:36:54 +0200

    Initial Commit(13.05.17 : 17:36 MainFrame und FrameCore

--- src/de/Kilso4dev/predatorPreySimulation/MainFrame.java
+++ src/de/Kilso4dev/predatorPreySimulation/MainFrame.java
@@ -1,7 +1,19 @@
 package de.Kilso4dev.predatorPreySimulation;
 
+import de.Kilso4dev.predatorPreySimulation.window.FrameCore;
+
+import javax.swing.*;
+import java.awt.*;
+
 public class MainFrame {
     public static void main(String[] args) {
-
+        FrameCore mainWindow = new FrameCore();
+        mainWindow.setTitle("Simulation einer Räuber-Beute Beziehung");
+        mainWindow.setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
+        mainWindow.setBounds((int) ((Toolkit.getDefaultToolkit().getScreenSize().getWidth()) - 1100) /2,
+                (int) (Toolkit.getDefaultToolkit().getScreenSize().getHeight() -600) /2,
+                1100, 700 );
+        mainWindow.setResizable(false);
+        mainWindow.setVisible(true);
     }
 }
--- src/de/Kilso4dev/predatorPreySimulation/window/FrameCore.java
+++ src/de/Kilso4dev/predatorPreySimulation/window/FrameCore.java
@@ -1,6 +1,17 @@
 package de.Kilso4dev.predatorPreySimulation.window;
 
 import javax.swing.*;
+import java.awt.*;
 
 public class FrameCore extends JFrame {
+
+    private Container cMainWindow;
+
+    public FrameCore() {
+        cMainWindow = getContentPane();
+        cMainWindow.setLayout(null);
+
+        //
+    }
+
 }
