986393f14a646a97b916e57d1d4db08010ef46ef
Robert Reuter <denib.qatlh@t-online.de>
2017-05-13 19:54:42 +0200

    Initial Commit(13.05.17 : 17:36 MainFrame und FrameCore

--- src/de/Kilso4dev/predatorPreySimulation/window/FrameCore.java
+++ src/de/Kilso4dev/predatorPreySimulation/window/FrameCore.java
@@ -7,7 +7,44 @@ import java.awt.*;
 
 public class FrameCore extends JFrame {
 
+    private Container cMainWindow;
+    private final Font fAll = new Font("Times New Roman", Font.PLAIN, 15);
 
+    public FrameCore() {
+        cMainWindow = getContentPane();
+        cMainWindow.setLayout(null);
+        createHeadline();
+    }
 
+    private void createHeadline() {
+        createGenerations();
+        createFieldSize();
+        createAnimals();
+    }
 
+    private void createAnimals() {
+    }
+
+    private void createFieldSize() {
+    }
+
+    private void createGenerations() {
+        JLabel generationInfo = new JLabel("Anzahl der Generationen:");
+        generationInfo.setBounds(20, 5, 200, 49);
+        generationInfo.setVisible(true);
+        generationInfo.setFont(fAll);
+        cMainWindow.add(generationInfo);
+
+        JTextField generationInput = new JTextField("100");
+        generationInput.setBounds(20, 59, 150, 30);
+        generationInput.setVisible(true);
+        generationInput.setFont(fAll);
+        cMainWindow.add(generationInput);
+
+        JButton generationRandom = new JButton("Zufällige Anzahl");
+        generationRandom.setBounds(20, 95, 150, 49);
+        generationRandom.setVisible(true);
+        generationRandom.setFont(fAll);
+        cMainWindow.add(generationRandom);
+    }
 }
