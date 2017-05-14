2783e1de66c398571c926a9456f2c43622e14a51
BuildTools <unconfigured@null.spigotmc.org>
2017-05-14 13:13:15 +0200

    worked on the Simulation Core, added TODO to FrameCore Class

--- src/de/Kilso4dev/predatorPreySimulation/core/SimulationCore.java
+++ src/de/Kilso4dev/predatorPreySimulation/core/SimulationCore.java
@@ -4,22 +4,49 @@ public class SimulationCore {
 
     Animal[][] playingField;
 
+    SimulationThread simulation;
 
-    public SimulationCore(int gridX, int gridY, int predatores, int preys) {
+    public SimulationCore(int gridX, int gridY, int predators, int preys, int generations) {
         playingField = new Animal[gridX][gridY];
-        for (int i = 0; i < predatores; i++) {
+
+        //initialize predators
+        for (int i = 0; i < predators; i++) {
             playingField[(int) (Math.random() * playingField.length)][(int) (Math.random() * playingField[0].length)] = new Predator();
         }
 
         int x;
         int y;
 
+        //initialize Preys
         for (int i = 0; i < preys; i++) {
             do {
                 x = (int) (Math.random() * playingField.length);
                 y = (int) (Math.random() * playingField[0].length);
             } while (playingField[x][y] instanceof Predator);
+
             playingField[x][y] = new Prey();
+        }
+    }
+
+    public void startSimulation() {
+
+    }
+
+
+    //private class for the SimulationThread
+    private class SimulationThread extends Thread {
+        private int generations;
+
+        private Animal[][] playingField;
+
+        public SimulationThread(Animal[][] playingField, int generations) {
+            this.generations = generations;
+            this.playingField = playingField;
+        }
+
+
+        public void run() {
+
         }
     }
 }
--- src/de/Kilso4dev/predatorPreySimulation/window/FrameCore.java
+++ src/de/Kilso4dev/predatorPreySimulation/window/FrameCore.java
@@ -7,6 +7,7 @@ import java.awt.*;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 
+// TODO: Add A Testarea with a JScrollPane named outputArea to display the output of every Run/ Generation
 public class FrameCore extends JFrame {
 
     private Container cMainWindow;
