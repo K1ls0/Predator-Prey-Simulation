eef834a55fed65c6f38c00a44c73323d0bd737ea
BuildTools <unconfigured@null.spigotmc.org>
2017-05-18 18:45:44 +0200

    added Components to add a Diagram

--- src/de/Kilso4dev/predatorPreySimulation/window/FrameCore.java
+++ src/de/Kilso4dev/predatorPreySimulation/window/FrameCore.java
@@ -14,7 +14,7 @@ import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 import java.text.DecimalFormat;
 
-// TODO: Add A Textarea with a JScrollPane named outputArea to display the output of every Run/ Generation //ok
+
 public class FrameCore extends JFrame {
 
     private SimulationCore simulationCore;
